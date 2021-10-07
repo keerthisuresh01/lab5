9cc54d22b7b43d2c3d34f30322b1bbbef18124a4
Keerthi <80613202+keerthisuresh01@users.noreply.github.com>
2021-10-07 11:02:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 var http=require('http');
 var url=require('url');
-const PORT=process.env.PORT || 3000;
+var fs=require('fs');
 var querystring=require('querystring');
-var server=http.createServer(function(request,response){
-    var query=url.parse(request.url).query;
-    var name=querystring.parse(query)['username'];
-    var password=querystring.parse(query)["password"];
-    response.write("<h1>My Info:</h1>");
-    response.write("<h4>Author: Keerthi S</h4>");
-    response.write("<h4>Roll no: 19ITR041</h4>");
-    response.write("<h1>Server Information:</h1>");
-    response.write("<h4>URL: "+request.url+"</h4>");
-    response.write("<h4>Query: "+query+"</h4>");
-    response.write("<h4>User: "+name+"</h4>");
-    response.write("<h4>Password: "+password+"</h4>");
-    response.write("<p style='color:red'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et possimus nulla voluptas nihil nostrum eaque, error velit exercitationem dolor harum culpa vitae corrupti eligendi ducimus consectetur, assumenda architecto ratione! At facere, sit iure soluta in voluptas consectetur dolore vel ipsum perferendis commodi alias quas maiores corporis dicta repudiandae obcaecati accusamus harum rem. Commodi eligendi corporis animi laboriosam amet quis?</p>")
-    response.end();
-}).listen(PORT);
-console.log(PORT);
 
+const port=process.env.port || 3000;
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-type':'text/html'});
+    fs.readFile('./index.html',null,function(err,data){
+        console.log("inside err check");
+        if(err){
+            res.writeHead(404);
+            res.write("File not found");
+        }
+        else{
+            res.write(data);
+            res.end();
+        }
+    })
+    var path=url.parse(req.url).pathname;
+    var query=url.parse(req.url).query;
+    res.write("<div class='container'><h3>My Info:</h3>");
+    res.write("<h5 class='pl-5'>Author: Keerthi S</h5>");
+    res.write("<h5 class='pl-5'>Roll no: 19ITR041</h5>");
+    res.write("<h3>Server Information:</h3>");
+    res.write("<h5 class='pl-5'>URL: "+req.url+"</h5>");
+    res.write("<h5 class='pl-5'>Query: "+query+"</h5>");
+    res.write("<h5 class='pl-5'>Pathname: "+path+"</h5></div>");
+            
+}).listen(port);
+    
+console.log(port);
